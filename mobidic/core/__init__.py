c20bf547d0fdf019016bf0ef2e90fdd9e7530803
Matteo Masi <4078033+matteomasi@users.noreply.github.com>
2025-10-27 14:16:47 +0100

    Moved interpolation to core

--- mobidic/core/__init__.py
+++ mobidic/core/__init__.py
@@ -1,10 +1,18 @@
 """Core simulation components for MOBIDIC hydrological model."""
 
+from mobidic.core import constants
 from mobidic.core.soil_water_balance import soil_mass_balance, capillary_rise
 from mobidic.core.routing import hillslope_routing, linear_channel_routing
 from mobidic.core.simulation import Simulation, SimulationState, SimulationResults
+from mobidic.core.interpolation import (
+    precipitation_interpolation,
+    temperature_interpolation,
+    create_grid_coordinates,
+)
+from mobidic.core.pet import calculate_pet
 
 __all__ = [
+    "constants",
     "soil_mass_balance",
     "capillary_rise",
     "hillslope_routing",
@@ -12,4 +20,8 @@ __all__ = [
     "Simulation",
     "SimulationState",
     "SimulationResults",
+    "precipitation_interpolation",
+    "temperature_interpolation",
+    "create_grid_coordinates",
+    "calculate_pet",
 ]
