9bc1a7c20a4a2cb69359fc771d3180672c88327c
Matteo Masi <4078033+matteomasi@users.noreply.github.com>
2026-04-22 09:28:29 +0200

    Energy balance API exposed

--- mobidic/core/__init__.py
+++ mobidic/core/__init__.py
@@ -5,6 +5,14 @@ from mobidic.core.soil_water_balance import soil_mass_balance, capillary_rise
 from mobidic.core.routing import hillslope_routing, linear_channel_routing
 from mobidic.core.reservoir import ReservoirState, reservoir_routing
 from mobidic.core.groundwater import groundwater_linear
+from mobidic.core.energy_balance import (
+    compute_energy_balance_1l,
+    diurnal_radiation_cycle,
+    energy_balance_1l,
+    saturation_specific_humidity,
+    solar_hours,
+    solar_position,
+)
 from mobidic.core.simulation import Simulation, SimulationState, SimulationResults
 from mobidic.core.interpolation import (
     precipitation_interpolation,
@@ -21,6 +29,12 @@ __all__ = [
     "ReservoirState",
     "reservoir_routing",
     "groundwater_linear",
+    "compute_energy_balance_1l",
+    "diurnal_radiation_cycle",
+    "energy_balance_1l",
+    "saturation_specific_humidity",
+    "solar_hours",
+    "solar_position",
     "Simulation",
     "SimulationState",
     "SimulationResults",
